65388e72896d07612d877d094f7bd07dd597fb79
surajgirioffl <surajkumargiri2002@gmail.com>
2022-07-03 21:10:45 +0530

    added option to add or remove index in message in message bomber

--- External Modules/02_bulk_message_bomber.py
+++ External Modules/02_bulk_message_bomber.py
@@ -15,14 +15,19 @@ i = 1
 
 originalString = input("Write any string: ")
 number = int(input("How many messages do you want to send: "))
+print("Do you want to append index in your string (yes or no) : ")
+choice = input()
 print("\033[1;31mMessage will be printed at your current cursor position. So, Place your cursor wherever you want to write messages...")
 print("\033[1;32mYou have 5 seconds to change the cursor position......")
+
+string = originalString  # initially string to be printed will originalString
 
 while i <= number:
     if i == 1:
         sleep(5)
-    string = addSubStringAfterIndex(
-        originalString, len(originalString), str(i))
+    if(choice == "yes" or choice == "Yes"):
+        string = addSubStringAfterIndex(
+            originalString, len(originalString), str(i))
     write(string)
     press("enter")
     i += 1
