481af3e88c5866e43bee25b260a745e5d9c33d32
surajgirioffl <surajkumargiri2002@gmail.com>
2022-07-06 16:47:03 +0530

    removed unwanted condition to check in each iteration of loop & pause the script for 5 before entering in infinite loop & added author info in __docs__

--- External Modules/02.02_auto_reload_page.py
+++ External Modules/02.02_auto_reload_page.py
@@ -1,11 +1,15 @@
 """
-    Script to auto reload a webpage by pressing ctrl + r.
+    Script to auto reload a webpage by pressing ctrl + r every second.
     Used of external module pyautogui.
+     *author: Suraj Kumar Giri.
+     *Date: 06-07-2022 (Last Updated)
+     *Time: 12:48:20
 """
 from pyautogui import hotkey
 from time import sleep
-i = 0
-while(i <= 1000):
-    if(i == 0):
-        sleep(5)  # to sleep for 5 seconds
+# to sleep for 5 seconds for switching to respective webpage to implement the script
+sleep(5)
+
+while(True):
     hotkey("ctrl", "r")
+    # sleep(0.3)
