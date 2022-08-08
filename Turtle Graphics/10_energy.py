5b7abc87d326dafb7c9a960a0bbd1fe31bfeba4f
surajgirioffl <surajkumargiri2002@gmail.com>
2022-08-08 16:35:16 +0530

    :new:New: add new file of turtle graphics
    add partially completed program of turtle graphics (draw light emitting
    source of energy)
    
    Signed-off-by: @surajgirioffl

---
+++ Turtle Graphics/10_energy.py
@@ -0,0 +1,9 @@
+import turtle
+t = turtle.Turtle()  # creating instance of class Turtle
+turtle.Screen().bgcolor("black")
+
+t.color((137, 99, 99))
+t.begin_fill()
+t.circle(20)
+t.end_fill()
+turtle.done()
