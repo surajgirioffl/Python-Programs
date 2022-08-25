3eacdf2de0dff20d6347d14cd2e2e08b60250e10
surajgirioffl <surajkumargiri2002@gmail.com>
2022-08-25 23:56:45 +0530

    :new:New: Add program for basics of `for` loop
    Add program to understand the basics of for loop.
    
    Signed-off-by: @surajgirioffl

---
+++ Starting With Python (Foundation)/04_Loops/03_for_loop.py
@@ -0,0 +1,9 @@
+"""
+    *Basic understanding of for loop.*
+"""
+
+for i in range(10):  # it is same as for i in range(0,10)
+    print(i)
+
+# initially,  first item of sequence (range(0,10)) is assigned to the iterating variable i.
+# and after each iteration, the next item of sequence (range(0,10)) is assigned to the iterating variable
