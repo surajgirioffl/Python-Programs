c624cdca6bbbc2b7cc795268f1de063b6e268a67
surajgirioffl <surajkumargiri2002@gmail.com>
2022-09-06 23:39:35 +0530

    :new:Add: add one more way of string formatting
    Add one more way of string formatting in python (F-string).
    
    Signed-off-by: @surajgirioffl

---
+++ Starting With Python (Foundation)/06_Strings/01-String Formatting/04_F-String_basic.py
@@ -0,0 +1,10 @@
+"""
+    Strong formatting using most popular and most used format 'f-string'.
+"""
+# it's simple just put 'f' before the string and inside curly braces write the variable name that you want to replace with it's value.
+name = "Suraj Giri"
+roll = 10
+subject = "Python"
+topic = "String Formatting"
+print(f"Name: {name} && Roll: {roll} && Subject: {subject} && Topic: {topic}")
+# Output: Name: Suraj Giri && Roll: 10 && Subject: Python && Topic: String Formatting
---
+++ Starting With Python (Foundation)/06_Strings/01-String Formatting/05_Advanced_F-String.py
@@ -0,0 +1,6 @@
+# Printing coma separated values using f-string syntax
+number = 100238
+print(f"{number:,}")
+# output: 100,238
+
+# some more f-string formatting examples
